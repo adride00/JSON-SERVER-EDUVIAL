cc7afb0661d69c3952045b976d553bf3c9594457
adride00 <adride@outlook.com>
2024-08-13 00:08:50 -0600

    chore: Update json-server dependency to version 0.17.4

--- server.js
+++ server.js
@@ -1,42 +1,11 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-
-server.use((req, res, next) => {
-  if (req.method === 'GET' && req.path === '/blog') {
-    const page = parseInt(req.query._page, 10) || 1;
-    const perPage = parseInt(req.query._per_page, 10) || 10;
-
-    const start = (page - 1) * perPage;
-    const end = start + perPage;
-
-    const data = router.db.get('blog').value();
-
-    const paginatedData = data.slice(start, end);
-
-    const totalItems = data.length;
-    const totalPages = Math.ceil(totalItems / perPage);
-
-    const response = {
-      first: 1,
-      prev: page > 1 ? page - 1 : null,
-      next: page < totalPages ? page + 1 : null,
-      last: totalPages,
-      pages: totalPages,
-      items: totalItems,
-      data: paginatedData,
-    };
-
-    res.json(response);
-  } else {
-    next();
-  }
-});
-
-server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('', router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
